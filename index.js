51fbf86f55ff42120f3d1a3157ff3762e2d7d858
zachary oliver <zspoliver@gmail.com>
2024-03-20 18:05:02 -0400

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+const employee = {
+    name: "Stevie",
+    streetAddress: "123 Meow Street"
+}
+
+function updateEmployeeWithKeyAndValue(empObj, key, value) {
+    const newObj = { ...empObj }
+    newObj[key] = value 
+
+    return newObj
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(empObj, key, value) {
+    empObj[key] = value
+    
+    return empObj
+}
+
+function deleteFromEmployeeByKey(empObj, key) {
+    const newObj = { ...empObj } 
+
+    delete newObj[key]
+
+    return newObj
+}
+
+function destructivelyDeleteFromEmployeeByKey(empObj, key) {
+
+    delete empObj[key]
+
+    return empObj
+}
